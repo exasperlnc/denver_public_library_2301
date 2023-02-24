e0f474facfff290fa7b3d163cd6b9b2939b8c133
Logan Cole <emtlogancole@gmail.com>
2023-02-24 10:10:40 -0600

    feat:book class

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,9 @@
 class Book
-  
+  attr_reader :author_first_name, :author_last_name, :title, :publication_date
+  def initialize(book_att)
+    @author_first_name = book_att[:author_first_name]
+    @author_last_name = book_att[:author_last_name]
+    @title = book_att [:title]
+    @publication_date = book_att[:publication_date]
+  end
 end
---
+++ spec/book_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+require './lib/book'
+
+RSpec.describe Book do
+  before(:each) do
+    @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+  end
+  it 'exists' do
+    expect(@book).to be_a Book
+  end
+
+  it 'has author names' do
+    expect(@book.author_first_name).to eq("Harper")
+    expect(@book.author_last_name).to eq("Lee")
+  end
+
+  it 'has title' do
+    expect(@book.title).to eq("To Kill a Mockingbird")
+  end
+
+  it 'has publication date' do
+    expect(@book.publication_date).to eq("July 11, 1960")
+  end
+
+end
