f3de79559238eaa298632697b91011559bbe9535
Logan Cole <emtlogancole@gmail.com>
2023-02-24 10:38:49 -0600

    pseudocode for library class

--- lib/library.rb
+++ lib/library.rb
@@ -1,3 +1,21 @@
+require './lib/author'
 class Library
- 
+  def initialize(library_att)
+    library_att
+    library_att
+    library_att
+    
+  end
+  
+  def add_author
+    #adds author to list of authors
+      #adds authors books to list of books
+  end
+
+  def publication_time_frame_for(author)
+    #takes author as argument
+    #returns hash with two key value pairs
+    #:start, which is the publication year of the first book
+    #:end, which points to publication year of last book
+  end
 end
